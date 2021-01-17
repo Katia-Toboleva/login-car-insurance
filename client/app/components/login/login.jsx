c1825ae4c4699aa52a92ac6f0f14c57cba1b1391
Katia-2020 <kateflights@gmail.com>
2021-01-17 17:49:03 +0000

    Login component with state manages by Reack Hooks. Styles and exported added as well

---
+++ client/app/components/login/login.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import {
+  Text,
+  Input,
+  Button,
+  Spinner,
+  Icon,
+} from '@components';
+
+import styles from './login.scss';
+
+const Login = ({ loginStatus, onLoginSubmit }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [typePassword, setInputType] = useState(true);
+
+  const handleUsernameChange = (value) => {
+    setUsername(value);
+  };
+
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  };
+
+  const handleInputTypeClick = () => {
+    setInputType(!typePassword);
+  };
+
+  const handleSubmitButtonClick = () => {
+    onLoginSubmit({
+      username,
+      password,
+    });
+  };
+
+  return (
+    <div className={styles['login__wrapper']}>
+      <div className={styles.login}>
+        <div className={styles['login__title']}>
+          <Text
+            text="sign in"
+            transform="capitalize"
+            size="large"
+            weight="bold"
+            center
+          />
+        </div>
+        <div className={styles['login__body']}>
+          <Input
+            type="text"
+            placeholder="user name"
+            onChange={handleUsernameChange}
+          />
+          <div className={styles['login__password']}>
+            <Input
+              type={typePassword ? "password" : "text"}
+              placeholder="password"
+              onChange={handlePasswordChange}
+            />
+            <div className={styles['login__password__icon']}>
+              <Icon
+                icon={typePassword ? "closedEye" : "openEye"}
+                color={typePassword ? "grey" : "red"}
+                size="medium"
+                onClick={handleInputTypeClick}
+              />
+            </div>
+          </div>
+        </div>
+        {loginStatus === "pending" && <Spinner />}
+        {loginStatus === "rejected" && <div>Error!</div>}
+        <div className={styles['login__controls']}>
+          <Button
+            type="submit"
+            text="sign in"
+            size="medium"
+            onClick={username && password ? handleSubmitButtonClick : null}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
