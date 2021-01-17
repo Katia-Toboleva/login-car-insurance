e6820ce6e17425ba5880e742322351b4c2fbe540
Katia-2020 <kateflights@gmail.com>
2021-01-17 17:20:10 +0000

    Button component with styles and export added

---
+++ client/app/components/button/button.jsx
@@ -0,0 +1,50 @@
+import React, { useState }  from 'react';
+import classnames from 'classnames/bind';
+import { Text } from '@components';
+import styles from './button.scss';
+
+const cx = classnames.bind(styles);
+
+const Button = (props) => {
+  const [ isHovered, setHovered ] = useState(false);
+  const {
+    text,
+    size,
+    type,
+    onClick,
+  } = props;
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  }
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  }
+
+  const handleButtonClick = () => {
+    onClick(type);
+  }
+
+  return (
+    <div
+      className={cx('button', {
+        [`button--type-${type}`]: type,
+        'button--hovered': isHovered,
+        'button--disabled': !onClick,
+      })}
+      onClick={handleButtonClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <Text
+        text={text}
+        size={size}
+        transform="uppercase"
+        color="white"
+      />
+    </div>
+  );
+};
+
+export default Button;
