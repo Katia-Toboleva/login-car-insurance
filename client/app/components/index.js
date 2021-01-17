33d42bc9e707fba8846bd63c6dcf40e9f49d5fcf
Katia-2020 <kateflights@gmail.com>
2021-01-17 17:47:50 +0000

    export of the components for using alias in imports

---
+++ client/app/components/index.js
@@ -0,0 +1,8 @@
+export { default as Text } from './text';
+export { default as Button } from './button';
+export { default as Icon } from './icon';
+export { default as Input } from './input';
+export { default as Spinner } from './spinner';
+export { default as Page } from './page';
+export { default as Login } from './login';
+export { default as Policy } from './policy';
