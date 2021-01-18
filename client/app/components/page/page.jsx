8d73ec14a275d0c477b65506e385ad2d1c88a63f
Katia-2020 <kateflights@gmail.com>
2021-01-18 15:59:01 +0000

    Page component for ReactRouter

---
+++ client/app/components/page/page.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { LoginContainer, PolicyContainer } from '../../containers';
+
+const IsLoggedin = ({ children }) => {
+  const accessToken = localStorage.getItem('access-token');
+
+  if (accessToken) {
+    return children;
+  }
+
+  return (
+    <Redirect to='/login' />
+  )
+}
+
+const Page = () => {
+  return (
+    <Switch>
+      <Route path="/login">
+        <LoginContainer />
+      </Route>
+      <Route path="/policy">
+        <IsLoggedin>
+          <PolicyContainer />
+        </IsLoggedin>
+      </Route>
+      <Route path="/">
+        <Redirect to="/login" />
+      </Route>
+    </Switch>
+  )
+};
+
+export default Page;
