7383b8d0185f9f4db1e1fa18ca88f842cc0a78e9
Katia-2020 <kateflights@gmail.com>
2021-01-18 15:23:12 +0000

    PolicyContainer component for connextion with Redux store. Exports added as well

---
+++ client/app/containers/policy-container/policy-container.jsx
@@ -0,0 +1,37 @@
+import React, {useEffect} from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Policy, Spinner } from '@components';
+import * as actions from './state/policy-container.actions';
+
+const PolicyContainer = ({ state, actions }) => {
+  const { fetchDataRequestStatus, policyDetails} = state;
+
+  useEffect(() => {
+    actions.fetchPolicy();
+  }, [])
+
+  return (
+    <>
+      {fetchDataRequestStatus === "rejected" && <div>Error!</div> }
+      {fetchDataRequestStatus === "pending" && <Spinner />}
+      {fetchDataRequestStatus === "success" && (
+        <Policy
+          policyDetails={policyDetails}
+        />
+      )}
+    </>
+  );
+};
+
+const mapStateToProps = (store) => ({
+  state: {
+    ...store.policyReducer,
+  },
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    actions: bindActionCreators(actions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PolicyContainer);
