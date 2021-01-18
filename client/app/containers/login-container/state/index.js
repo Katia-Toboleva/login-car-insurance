8f630a3d4ee5714128499821bb76cd29e8427332
Katia-2020 <kateflights@gmail.com>
2021-01-18 15:21:04 +0000

    LoginContainer -> added exports for constants, actions and reducers

---
+++ client/app/containers/login-container/state/index.js
@@ -0,0 +1,3 @@
+export { default as reducer } from './login-container.reducers';
+export { default as constants } from './login-container.constants';
+export { default as actions } from './login-container.actions';
