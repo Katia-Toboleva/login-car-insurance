c26667ed9a9c1d592083de49ab48f96074e2e32c
Katia-2020 <kateflights@gmail.com>
2021-01-18 15:58:25 +0000

    Reducers imported to the Redux store

---
+++ client/app/state/reducers.js
@@ -0,0 +1,7 @@
+import loginReducer from '../containers/login-container/state/login-container.reducers';
+import policyReducer from '../containers/policy-container/state/policy-container.reducers';
+
+export default {
+  loginReducer,
+  policyReducer,
+};
