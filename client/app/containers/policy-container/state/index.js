9a4137fa49a13a842fe945630423a27b3f6c3ce7
Katia-2020 <kateflights@gmail.com>
2021-01-18 15:57:11 +0000

    PolicyContainer state -> exports  added

---
+++ client/app/containers/policy-container/state/index.js
@@ -0,0 +1,3 @@
+export { default as reducer } from './policy-container.reducers';
+export { default as constants } from './policy-container.constants';
+export { default as actions } from './policy-container.actions';
