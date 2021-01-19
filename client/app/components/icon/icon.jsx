bb9abb4e73de96cac17f9a58ad3d04db12faba5b
Katia-2020 <kateflights@gmail.com>
2021-01-19 12:01:29 +0000

    Icon -> added propTypes and defaultProps for the component

--- client/app/components/icon/icon.jsx
+++ client/app/components/icon/icon.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 
 import * as glyphs from './svg';
-
 import styles from './icon.scss';
 
 const cx = classnames.bind(styles);
@@ -24,5 +24,19 @@ const Icon = ({
     <i dangerouslySetInnerHTML={{ __html: glyphs[icon] }} />
   </div>
 );
+
+Icon.defaultProps = {
+  icon: 'closedEye',
+  size: 'grey',
+  color: 'medium',
+  onClick: () => { },
+};
+
+Icon.propTypes = {
+  icon: PropTypes.oneOf(['closedEye', 'openEye']),
+  size: PropTypes.oneOf(['small', 'medium']),
+  color: PropTypes.oneOf(['grey', 'red']),
+  onClick: PropTypes.func,
+};
 
 export default Icon;
