f82e3cc6b8cebc0696b9fbafa2b9ab1ecd29c8d0
Katia-2020 <kateflights@gmail.com>
2021-01-20 19:21:37 +0000

    added setup from jest and webpack alias

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   setupFiles: ["<rootDir>/jest/globals.js", "jest-localstorage-mock"],
   setupFilesAfterEnv: ["<rootDir>setup-tests.js"],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "^@components(.*)$": "<rootDir>/client/app/components$1",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/client/app/__mocks__/fileMock.js",
     "\\.(css|scss)$": "identity-obj-proxy"
   },
   snapshotSerializers: ["enzyme-to-json/serializer"],
