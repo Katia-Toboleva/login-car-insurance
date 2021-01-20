f0295862bdc272f5d73fce18b50ab36b50bccad0
Katia-2020 <kateflights@gmail.com>
2021-01-20 00:26:29 +0000

    Enzyme setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   setupFiles: ["<rootDir>/jest/globals.js", "jest-localstorage-mock"],
+  setupFilesAfterEnv: ["<rootDir>setup-tests.js"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|scss)$": "identity-obj-proxy"
+  },
+  snapshotSerializers: ["enzyme-to-json/serializer"],
 };
